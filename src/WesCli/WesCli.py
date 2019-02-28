4680dbdd226c927183d1ffe880681e0add60fe65
Thiago Albuquerque <thiagofga@gmail.com>
2019-02-28 18:06:52 +0000

    [#21] getEffectiveConf()

--- src/WesCli/WesCli.py
+++ src/WesCli/WesCli.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 import yaml
+from jinja2 import Template
 
 
 def loadYaml(filename):
@@ -9,3 +10,24 @@ def loadYaml(filename):
         
         return yaml.safe_load(f)
 
+
+def getEffectiveConf(conf):
+    
+    inputTemplate   = conf['inputTemplate']
+    sites           = conf['sites']
+    
+    template = Template(inputTemplate)
+
+    def renderSite(s):
+        
+        return {
+            
+            'url'   : s['url']
+           ,'input' : template.render(s['inputTemplateParams'])
+        }
+        
+    return {
+        
+        'workflow'  : conf['workflow']
+       ,'sites'     : [ renderSite(s) for s in sites ]
+    }
--- test/Test.py
+++ test/Test.py
@@ -2,10 +2,16 @@
 
 import unittest
 from pprint import pprint
-from WesCli.WesCli import loadYaml
+from WesCli.WesCli import loadYaml, getEffectiveConf
+from jinja2 import Template
 
 
 class Test(unittest.TestCase):
+    
+    def setUp(self):
+        
+        self.maxDiff = None                 # Diff is 709 characters long. Set self.maxDiff to None to see it.
+    
 
     def test_loadYaml(self):
         
@@ -13,7 +19,43 @@ class Test(unittest.TestCase):
         
         pprint(yaml)
         
+    
+    def test_renderTemplate(self):
         
+        template = Template('Hello {{ name }}!')
+        txt = template.render(name='world')
+        
+        self.assertEquals(txt, 'Hello world!')
+
+
+    def test_getEffectiveConf(self):
+            '''
+            {'inputTemplate': '{ "input": {   "class": "File",   "location": "{{input}}" } '
+                              '}',
+                              
+             'sites': [{'inputTemplateParams': {'input': 'file:///tmp/hashSplitterInput/test1.txt'},
+                        'site': 'http://localhost:8080/ga4gh/wes/v1'},
+                       {'inputTemplateParams': {'input': 'file:///tmp/hashSplitterInput/test2.txt'},
+                        'site': 'http://localhost:8080/ga4gh/wes/v1'}],
+                        
+             'workflow': 'https://github.com/fgypas/cwl-example-workflows/blob/master/hashsplitter-workflow.cwl'}
+            '''
+        
+            yaml = loadYaml('examples/sites.yaml')
+            
+            self.assertEqual(getEffectiveConf(yaml), {
+                
+                 'workflow': 'https://github.com/fgypas/cwl-example-workflows/blob/master/hashsplitter-workflow.cwl'
+                
+                ,'sites': [
+                            { 'input' : '{ "input": {   "class": "File",   "location": "file:///tmp/hashSplitterInput/test1.txt" } }'
+                            , 'url'   : 'http://localhost:8080/ga4gh/wes/v1'
+                            }
+                          , { 'input' : '{ "input": {   "class": "File",   "location": "file:///tmp/hashSplitterInput/test2.txt" } }'
+                            , 'url'   : 'http://localhost:8080/ga4gh/wes/v1'
+                            }
+                          ]
+            })
         
         
 
