f835453b74e7dbb2ae1d6f100efa13257e36c925
Thiago Albuquerque <thiagofga@gmail.com>
2019-03-05 12:30:02 +0000

    [#21] ArgParser

---
+++ src/WesCli/ArgParser.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+
+import sys
+from docopt import docopt
+
+
+def main():
+    
+    args = getOpts(sys.argv[1:])
+    
+    print(args)
+    
+    
+def getOpts(args):
+    
+    doc = """
+Usage:
+    wes run <runSpec>
+
+
+Options:
+  -h --help                          Shows this screen.
+
+
+"""
+    
+    
+    return docopt(doc, argv=args)
+    
+    
+
+
+if __name__ == '__main__':
+    main()
