15d56446b26a34e9228234c9631f07a7f3f26fce
Sam Fenton <45848090+SamFent@users.noreply.github.com>
2019-08-28 09:40:13 +0100

    Updated for new wes download option

--- src/WesCli/ArgParser.py
+++ src/WesCli/ArgParser.py
@@ -18,12 +18,14 @@ Usage:
     wes run <runSpec>
     wes status [-w|--watch]
     wes get <url>
-    wes download [-p] <url>
+    wes download [-p|--progress] [-d <location>|--destination=<location>] <url>
     wes upload <filename> <url>
 
 Options:
-  -h --help                          Shows this screen.
-  -p                                 Display progress bar when downloading file
+  -h --help                                                     Shows this screen.
+  -p --progress                                                 Display progress bar when downloading file
+  -d <location> --destination <location>                        Designate a destination when downloading file
+
 """
 
     return docopt(doc, argv=args)
