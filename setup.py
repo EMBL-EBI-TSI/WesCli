b0e68ee4a7181134229590a9d9f131b1fd7e79b5
Sam Fenton <45848090+SamFent@users.noreply.github.com>
2019-08-30 15:36:03 +0100

    Updated to allow pip install
    Added python and dependencies requirements section

--- setup.py
+++ setup.py
@@ -8,7 +8,17 @@ setup(
     version = "1.0",
     packages = find_packages('src'),
     package_dir = {'':'src'},
-    
+    python_requires=">=3.6",
+    install_requires=[
+        "pyyaml",
+        "requests",
+        "docopt",
+        "pydash",
+        "funcy",
+        "mypy",
+        "nose",
+        "progressbar2"
+    ],
 
     entry_points = {
         'console_scripts': [
